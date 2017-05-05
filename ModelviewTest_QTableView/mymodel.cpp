528d73b5fde7a0fcf58bcb60537f674e8319f419
humadivinty <humadivinity@yahoo.com>
2017-05-05 16:56:46 +0800

    第二次修改
    修改data()方法，观察role参数的作用

--- ModelviewTest_QTableView/mymodel.cpp
+++ ModelviewTest_QTableView/mymodel.cpp
@@ -1,5 +1,9 @@
 #include"mymodel.h"
 #include<QVariant>
+#include<QDebug>
+#include<QFont>
+#include<QBrush>
+
 
 MyModel::MyModel(QObject *parents)
     :QAbstractTableModel(parents)
@@ -24,10 +28,47 @@ int MyModel::columnCount(const QModelIndex &parent) const
 
 QVariant MyModel::data(const QModelIndex &index, int role) const
 {
-    if(role == Qt::DisplayRole)
+    int row = index.row();
+    int col = index.column();
+    qDebug()<<QString("row %1, col%2, role %3").arg(row).arg(col).arg(role);
+
+    switch(role)
     {
-        //return QString("Row%1 , Column%2").arg(index.row()+1).arg(index.column()+1);
-        return QString("%1").arg(m_chData[index.row()][index.column()]);
+        case Qt::DisplayRole:
+            if(row ==0 && col ==1) return QString("<--left");
+            if(row == 1 && col == 1) return QString("right-->");
+
+            return  QString("Row%1, Column%2").arg(row + 1).arg(col +1);
+    case Qt::FontRole:
+            if (row == 0 && col == 0) //change font only for cell(0,0)
+            {
+                QFont boldFont;
+                boldFont.setBold(true);
+                return boldFont;
+            }
+            break;
+        case Qt::BackgroundRole:
+
+            if (row == 1 && col == 2)  //change background only for cell(1,2)
+            {
+                QBrush redBackground(Qt::red);
+                return redBackground;
+            }
+            break;
+        case Qt::TextAlignmentRole:
+
+            if (row == 1 && col == 1) //change text alignment only for cell(1,1)
+            {
+                return Qt::AlignRight + Qt::AlignVCenter;
+            }
+            break;
+    case Qt::CheckStateRole:
+
+        if (row == 1 && col == 0) //add a checkbox to cell(1,0)
+        {
+            return Qt::Checked;
+        }
     }
+
     return QVariant();
 }
